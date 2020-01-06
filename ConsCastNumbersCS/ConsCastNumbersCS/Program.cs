41c4feb69ac74cd1ceacdba2944416dda7872f66
Rika Takahashi <rikat@microsoft.com>
2020-01-06 17:27:29 +0900

    initializing repository

---
+++ ConsCastNumbersCS/ConsCastNumbersCS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsCastNumbersCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 27;
+            float f;
+            f = i;
+            Console.WriteLine("float value is {0}", f);
+            Console.ReadLine();
+        }
+    }
+}
